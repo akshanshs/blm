b7f89f9cd666ba2981f36df22396209a2e295cf2
Akshansh <akshanshz@gmail.com>
2026-01-19 21:56:30 +0100

    setup package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    description="US Visa prediction pipeline",
+    author="AK",
+    packages=find_packages()
+)
