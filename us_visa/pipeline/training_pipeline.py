efee7242cabfbdc6f7ffcfe3fb57662c9309c1d1
Akshansh <akshanshz@gmail.com>
2026-01-23 09:28:41 +0100

    run_pipeline method added with data ingestion in it. An update in training_pipeline

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -14,10 +14,25 @@ class TrainPipeline:
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
+        """
+        This method of TrainPipeline class is responsible for data ingestion
+        :return:
+        """
         try:
             logging.info(f"Starting data ingestion from mongodb")
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
             return data_ingestion_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    def run_pipeline(self) -> None:
+        """
+        This method of TrainPipeline class is responsible for running complete pipeline
+        :return:
+        """
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+
         except Exception as e:
             raise USvisaException(e, sys)
